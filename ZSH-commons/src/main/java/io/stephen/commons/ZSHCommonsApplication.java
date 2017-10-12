24d5ed2fc2b2e5298384e55841012fa6713c44d9
stephen_zhou <stephen_zho@126.com>
2017-10-12 22:53:08 +0800

    注册服务

--- ZSH-commons/src/main/java/io/stephen/commons/ZSHCommonsApplication.java
+++ ZSH-commons/src/main/java/io/stephen/commons/ZSHCommonsApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ZSHApplication {
+public class ZSHCommonsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZSHApplication.class, args);
+        SpringApplication.run(ZSHCommonsApplication.class, args);
     }
 }
