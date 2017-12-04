666f3688f2b21878dc8d60409f6cf3b21e328014
stephen_zhou <stephen_zho@126.com>
2017-12-04 23:06:44 +0800

    Dijkstra ， 广度优先算法

--- ZSH-security-demo/src/test/java/io/stephen/demo/graphTheory/Dijkstra.java
+++ ZSH-security-demo/src/test/java/io/stephen/demo/graphTheory/Dijkstra.java
@@ -1,82 +1,133 @@
 package io.stephen.demo.graphTheory;
 
+import io.swagger.models.auth.In;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
- * Dijkstra算法
- *
  * @author 10447
- * @since 2017/12/2
+ * @since 2017/12/3
  */
 public class Dijkstra {
 
-    public static Map<Integer,ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
 
-    public static ArrayDeque<Integer> queue = new ArrayDeque<>();
+    public static HashMap<String, ArrayList<HashMap>> map = new HashMap<String, ArrayList<HashMap>>();
+
+    public static HashMap parent = new HashMap();
+
+    public static HashMap consts = new HashMap();
+
+    public static ArrayList processd = new ArrayList();
 
 
 
     @Test
-    public void start() {
-        queue.add(1);
-        queue.add(2);
+    public void search() {
+        String key = findLowestCostNode();
 
-        String re = "我-";
-
-        while (!queue.isEmpty()) {
-            Integer i = queue.poll();
-
-            if (i == 10) {
-                re = re + "终点";
+        while (key != null) {
+            if ("终点".equals(key)) {
                 break;
-            } else {
-                re = re + i + "-";
+            }
 
-                ArrayList<Integer> list = map.get(i);
-                if (list == null) {
-                    break;
+            ArrayList<HashMap> ar = map.get(key);
+            for (HashMap ma : ar) {
+
+                Iterator iterator = ma.entrySet().iterator();
+
+                while (iterator.hasNext()) {
+                    HashMap.Entry ob = (HashMap.Entry) iterator.next();
+
+                    Integer va1 = (Integer) ob.getValue() + (Integer) consts.get(key);
+                    Integer va = (Integer) consts.get(ob.getKey());
+
+                    if (va1.compareTo(va) == -1) {
+                        consts.put(ob.getKey(), va1);
+                        parent.put(ob.getKey(),key);
+
+                    }
+
+                    processd.add(key);
                 }
-                queue.addAll(list);
+
             }
 
 
+            key = findLowestCostNode();
         }
 
-        System.out.println(re);
+        System.out.println(parent);
+
+
     }
+
+    public String findLowestCostNode() {
+        Iterator it = consts.entrySet().iterator();
+
+        Integer va = Integer.MAX_VALUE;
+        String ke = null;
+        while (it.hasNext()) {
+            HashMap.Entry ob = (HashMap.Entry) it.next();
+
+            String key = (String) ob.getKey();
+            Integer value = (Integer) ob.getValue();
+
+            if (va.compareTo(value) == 1 && !processd.contains(key)) {
+                va = value;
+                ke = key;
+            }
+
+        }
+
+
+        return ke;
+    }
+
+
 
     @Before
     public void before() {
-        ArrayList<Integer> list1 = new ArrayList<Integer>();
-        list1.add(3);
-        list1.add(4);
+        // ------------------
+        ArrayList<HashMap> qi = new ArrayList<HashMap>();
+        HashMap qia = new HashMap();
+        qia.put("a", 6);
+        HashMap qib = new HashMap();
+        qib.put("b", 2);
+        qi.add(qia);
+        qi.add(qib);
 
-        ArrayList<Integer> list2 = new ArrayList<Integer>();
-        list2.add(4);
-        list2.add(5);
-        ArrayList<Integer> list3 = new ArrayList<Integer>();
-        list3.add(6);
-        ArrayList<Integer> list4 = new ArrayList<Integer>();
-        list4.add(5);
-        list4.add(0);
-        list4.add(6);
+        ArrayList<HashMap> lista = new ArrayList<HashMap>();
+        HashMap amap = new HashMap();
+        amap.put("终点", 1);
+        lista.add(amap);
 
-        ArrayList<Integer> list5 = new ArrayList<Integer>();
-        list5.add(0);
+        ArrayList<HashMap> listb = new ArrayList<HashMap>();
+        HashMap bmap1 = new HashMap();
+        bmap1.put("a", 1);
+        HashMap bmap2 = new HashMap();
+        bmap2.put("终点", 5);
+        listb.add(bmap1);
+        listb.add(bmap2);
 
-        ArrayList<Integer> list6 = new ArrayList<Integer>();
-        list6.add(0);
+        map.put("起点",qi);
+        map.put("a",lista);
+        map.put("b",listb);
+        map.put("终点", null);
 
-        map.put(1, list1);
-        map.put(2, list2);
-        map.put(3, list3);
-        map.put(4, list4);
-        map.put(5, list5);
-        map.put(6, list6);
+        // ------------------
+        parent.put("a", "起点");
+        parent.put("b", "起点");
+        parent.put("终点","-");
 
+        // ---------------------
+        consts.put("a", 6);
+        consts.put("b", 2);
+        consts.put("终点", Integer.MAX_VALUE);
 
 
     }
